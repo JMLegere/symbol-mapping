a6512e8f017e5bf40e0fbcba4aeab0723ddb7f71
Jeremy Legere <jeremy.m.legere@gmail.com>
2025-07-24 16:31:03 -0300

    Fix test line lengths

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,13 +4,28 @@ from src.utils.merge_sources import merge_sources, prune_duplicates
 
 def test_prune_duplicates():
     entries = [
-        MapEntry(mappedIdType="CUSIP", mappedIdValue="1", sources=["http://a.com"]),
-        MapEntry(mappedIdType="CUSIP", mappedIdValue="1", sources=["http://a.com"]),
-        MapEntry(mappedIdType="CUSIP", mappedIdValue="1", sources=["http://b.com"]),
+        MapEntry(
+            mappedIdType="CUSIP",
+            mappedIdValue="1",
+            sources=["http://a.com"],
+        ),
+        MapEntry(
+            mappedIdType="CUSIP",
+            mappedIdValue="1",
+            sources=["http://a.com"],
+        ),
+        MapEntry(
+            mappedIdType="CUSIP",
+            mappedIdValue="1",
+            sources=["http://b.com"],
+        ),
     ]
 
     merged = merge_sources(entries)
     unique = prune_duplicates(merged)
 
     assert len(unique) == 1
-    assert {str(u) for u in unique[0].sources} == {"http://a.com/", "http://b.com/"}
+    assert {str(url) for url in unique[0].sources} == {
+        "http://a.com/",
+        "http://b.com/",
+    }
